7539f9e9ab7b3e0b1d704c0e190cbecd50391d21
tarushi-22 <55681384+tarushi-22@users.noreply.github.com>
2021-03-17 18:49:26 +0530

    Create dog-years.js
    project 2

---
+++ dog-years.js
@@ -0,0 +1,7 @@
+let age=prompt("Input your age");
+const earlyyear=2*10.5;
+age-=2;
+age*=4;
+const totalage=earlyyear+age;
+const name= 'Tarushi';
+console.log(`I am ${name}. My age is ${age} which is ${totalage} in dog years.`);
